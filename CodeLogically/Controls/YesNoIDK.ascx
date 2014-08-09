88a058bd33ad02a496b940b7a8bd60cb6c200c1a
leash246 <leash246@gmail.com>
2014-08-09 11:45:49 -0500

    Create Yes/No/IDK Control

---
+++ CodeLogically/Controls/YesNoIDK.ascx
@@ -0,0 +1,7 @@
+﻿<%@ Control Language="vb" AutoEventWireup="false" CodeBehind="YesNoIDK.ascx.vb" Inherits="CodeLogically.YesNoIDK" %>
+
+<h3><asp:Label runat="server" ID="lblQuestionText" /></h3>
+<br />&gt;&gt;
+<asp:RadioButton runat="server" ID="rbtYes" Text="Yes" GroupName="YNI" />
+<asp:RadioButton runat="server" ID="rbtNo" Text="No" GroupName="YNI" />
+<asp:RadioButton runat="server" ID="rbtIDKMyBFFJill" Text="I Don't Know" GroupName="YNI" />
---
+++ CodeLogically/Controls/YesNoIDK.ascx.vb
@@ -0,0 +1,40 @@
+﻿Public Class YesNoIDK
+    Inherits System.Web.UI.UserControl
+    Private _AllowIDK As Boolean = False
+    Public Property AllowIDK As Boolean
+        Get
+            Return _AllowIDK
+        End Get
+        Set(value As Boolean)
+            _AllowIDK = value
+            If value Then
+                rbtIDKMyBFFJill.Visible = True
+            Else
+                rbtIDKMyBFFJill.Visible = False
+            End If
+        End Set
+    End Property
+
+    Public Property QuestionText As String
+
+    ReadOnly Property YesID As String
+        Get
+            Return rbtYes.ClientID
+        End Get
+    End Property
+    ReadOnly Property NoID As String
+        Get
+            Return rbtNo.ClientID
+        End Get
+    End Property
+    ReadOnly Property IDKID As String
+        Get
+            Return rbtIDKMyBFFJill.ClientID
+        End Get
+    End Property
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+End Class
